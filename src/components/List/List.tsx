fa4a9747b265a86c19db28216cb50c9ae6d924ad
Оксана Юрьева <oksanayurieva@MacBook-Air-Oksana.local>
2024-09-26 23:36:17 +0500

    fixed list

--- src/components/List/List.tsx
+++ src/components/List/List.tsx
@@ -3,6 +3,7 @@ interface Item {
     icon?: string;
     color?: string;
     name: string;
+    id?: number,
     active?: boolean,
     style?: string,
 }
@@ -21,10 +22,10 @@ export default function List({ items, isRemovable, click }: ListProps): JSX.Elem
     return (
         <ul onClick={click} className={scss.list}>
             {
-                items.map((item, index) => (
-                    <li key={index} className={item.active ? scss.active : ""}>
+                items.map((item) => (
+                    <li key={item.id} className={item.active ? scss.active : ""}>
                         <i>
-                            {item.icon ? <img src={item.icon} alt="list-icon" style={{margin: item.style}}/> : <i className={`${scss.badge} ${scss[`badge_${item.color}`]}`}> </i>}
+                            {item.icon ? <img src={item.icon} alt="list-icon" style={{margin: item.style}}/> : <i className={scss.badge} style={{backgroundColor: item.color}}> </i>}
                         </i>
                         <span>{item.name}</span>
                     </li>
--- src/components/List/ListGeneral/ListGeneral.tsx
+++ src/components/List/ListGeneral/ListGeneral.tsx
@@ -1,33 +1,17 @@
 import List from "../List";
 import scss from './ListGeneral.module.scss';
-/* import dataBase from '../../../assets/database.json'; */
+import dataBase from '../../../assets/database.json';
+import { useState } from "react";
 
 export default function ListGeneral(): JSX.Element {
+    const [lists, setLists] = useState(
+        dataBase.lists.map((item) => (
+            { id: item.id, name: item.name, color: dataBase.colors.find(color => color.id === item.colorId)?.hex }
+        ))
+    );
     return (
         <div className={scss["list-general"]}>
-            <List items={[
-                {
-                    color: 'green',
-                    name: 'Покупки',
-                },
-                {
-                    color: 'blue',
-                    name: 'Фронтенд',
-                },
-                {
-                    color: 'pink',
-                    name: 'Фильмы и сериалы',
-                },
-                {
-                    color: 'light-green',
-                    name: 'Книги',
-                },
-                {
-                    color: 'grey',
-                    name: 'Личное',
-                },
-
-            ]} isRemovable />
+            <List items={lists} isRemovable />
         </div>
     )
 }
